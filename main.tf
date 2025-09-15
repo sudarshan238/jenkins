ac7c745ba7d0b2067db4efb031d39035fd26f68f
sudarshan238 <sudarshanyadav5678@gmail.com>
2025-09-15 11:38:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,8 @@
-
 resource "aws_instance" "dev" {
-  ami = "ami-0a94a1259c5779e00"
+  ami           = "ami-0a94a1259c5779e00"
   instance_type = "t2.micro"
 
+  tags = {
+    Name = "DevInstance"
+  }
 }
-
